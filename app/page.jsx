23b2d5e643b201cacd218738af37bb41c6a663f4
PeterFromHell <peterdangw515jd@gmail.com>
2023-01-02 22:15:53 +0800

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <div className='bg-[#1C1F24] w-screen h-screen'>
+            <h1 className='text-white'>Hello World!!</h1>
+        </div>
+    )
+}
+
+export default HomePage

3b49c12550a87942c8919d039507157e40185227
PeterFromHell <peterdangw515jd@gmail.com>
2023-01-08 14:29:02 +0800

    add pocketbase

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ const HomePage = () => {
         <div className='bg-[#1C1F24] w-screen h-screen flex flex-col items-center justify-center'>
             <h1 className='text-white text-[150px] py-0.5'>React 教學</h1>
             <div className='border border-white hover:bg-slate-400 rounded w-36 flex items-center justify-center'>
-                <Link className='text-[30px] text-white text-center' href='/entry'>開始</Link>
+                <Link className='text-[30px] text-white text-center' href='/lesson'>開始</Link>
             </div>
         </div>
     )
